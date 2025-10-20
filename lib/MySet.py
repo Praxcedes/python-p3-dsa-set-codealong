b507033243b5024b79efa2f5003939856095b48c
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-20 09:18:41 +0300

    python-p3-dsa-set-codealong

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,29 @@
 class MySet:
+    def __init__(self, elements=None):
+        # Initialize an empty dictionary
+        self.dictionary = {}
+        
+        # If a list of elements is given, add each to the dictionary
+        if elements:
+            for element in elements:
+                self.dictionary[element] = True
 
-    pass
+    def add(self, element):
+        # Add element as a key in dictionary
+        self.dictionary[element] = True
+
+    def delete(self, element):
+        # Safely remove element if it exists
+        if element in self.dictionary:
+            del self.dictionary[element]
+
+    def has(self, element):
+        # Check if element exists in dictionary
+        return element in self.dictionary
+
+    def size(self):
+        # Return number of elements
+        return len(self.dictionary)
+
+    def __repr__(self):
+        return f"MySet({list(self.dictionary.keys())})"
